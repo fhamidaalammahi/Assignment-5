77d4192bfa42d77fd5759858b58e1888bf1100f5
fhamidaalammahi <fahmidamahi59@gmail.com>
2026-03-09 01:58:56 +0600

    Implemented login functionality

--- script/login.js
+++ script/login.js
@@ -1,21 +1,13 @@
-console.log("login function")
-document.getElementById("login-btn")
-.addEventListener("click", function(){
-  const nameinput = document.getElementById("input login");
-  const username = nameinput.value;
-  console.log(username);
+const loginButton = document.getElementById("login-button");
 
-  const passinput =document.getElementById("input pass");
-  const pass = passinput.value;
-   console.log(pass);
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
 
-   if(username=="admin" && pass=="admin123"){
-    alert("login success");
-
-    window.location.assign("/home.html");
-   }
-   else{
-    alert("login Failed");
-    return;
-   }
+  if (username === "admin" && password === "admin123") {
+    window.location.assign("./home.html");
+  } else {
+    alert("Wrong username or password!");
+  }
 });
